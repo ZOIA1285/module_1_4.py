6b70be2164eba3381f2d59a28606ea13ad77ede3
ZOIA1285 <zoiamyshkova@gmail.com>
2024-10-21 20:17:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('"Это неважно, что медленно ты идёшь… главное — не останавливайся." - ')
+print(len(my_string)) # c методом 'len' была знакома ранее. Пункт 2.2 Вывести количество с
+                      # символов введённого текста
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ' , ''))
+print(my_string[0])
+print(my_string[-1])
+
